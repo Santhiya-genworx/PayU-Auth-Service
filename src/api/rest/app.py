4be0314786f4690b18257a1997fe1724d614f513
Santhiya-genworx <santhiyas@genworx.ai>
2026-03-05 23:47:05 +0530

    initial commit

---
+++ src/api/rest/app.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+from src.data.clients.database import Base, engine
+from src.api.rest.routes.user_router import user_router
+from src.api.rest.routes.health import health_router
+from src.api.rest.routes.refresh import refresh_router
+
+app_router = APIRouter()
+
+app_router.include_router(user_router)
+app_router.include_router(health_router)
+app_router.include_router(refresh_router)
+
+@app_router.on_event("startup")
+async def on_start():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
